59a62d40a665c5696099a43f97aaa767b7dbce64
Pablo <Pablocalvoespinar04@gmail.com>
2022-12-02 19:30:14 +0100

    Examen

---
+++ Gestion Tabacalera/Gestion Tabacalera/Form1.Designer.vb
@@ -0,0 +1,321 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Producto"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 65)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "TABAQUERAS"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(232, 65)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 20)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "MARCAS"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(521, 65)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(132, 20)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "CARACTERÍSTICAS"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(911, 65)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(69, 20)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "COMPRA"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(526, 107)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(111, 20)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "N CIGARRILLOS"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(682, 107)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(76, 20)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "NICOTINA"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(526, 209)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(58, 20)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "PRECIO"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(682, 209)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(96, 20)
+        Me.Label9.TabIndex = 10
+        Me.Label9.Text = "PROMOCIÓN"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(521, 150)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox2.TabIndex = 11
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(682, 150)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox3.TabIndex = 12
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(521, 248)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox4.TabIndex = 13
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(682, 248)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox5.TabIndex = 14
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(911, 107)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(103, 24)
+        Me.CheckBox1.TabIndex = 15
+        Me.CheckBox1.Text = "CARTONES"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(911, 169)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(150, 27)
+        Me.NumericUpDown1.TabIndex = 16
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(941, 225)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(82, 20)
+        Me.Label10.TabIndex = 17
+        Me.Label10.Text = "UNIDADES"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(20, 329)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(70, 20)
+        Me.Label11.TabIndex = 18
+        Me.Label11.Text = "PEDIDOS"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(291, 360)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(159, 29)
+        Me.Button1.TabIndex = 20
+        Me.Button1.Text = "REALIZAR PEDIDO"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(291, 419)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(159, 29)
+        Me.Button2.TabIndex = 21
+        Me.Button2.Text = "BORRAR PEDIDO"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"TABACALERA", "PHILIPS MORRIS", "JP MORGAN"})
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 107)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(151, 28)
+        Me.ComboBox1.TabIndex = 22
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(232, 107)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(167, 124)
+        Me.ListBox1.TabIndex = 23
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 20
+        Me.ListBox2.Location = New System.Drawing.Point(20, 360)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(150, 104)
+        Me.ListBox2.TabIndex = 24
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(502, 360)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(94, 29)
+        Me.Button3.TabIndex = 25
+        Me.Button3.Text = "Mostrar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 150)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(151, 109)
+        Me.PictureBox1.TabIndex = 26
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1162, 492)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "GESTION TABACALERA"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Gestion Tabacalera/Gestion Tabacalera/Form1.vb
@@ -0,0 +1,135 @@
+﻿Imports System.Net.Security
+
+Public Class Form1
+    Private Structure Pedidos
+        Public idPedido As Integer
+        Public cantidad As Integer
+        Public marca As String
+        Public total As Double
+
+    End Structure
+
+    Public x As Integer = 0
+    Dim pedido(10) As Pedidos
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.SelectedIndex = 0 Then
+            ListBox1.Items.Clear()
+            ListBox1.Items.Add("Fortuna")
+            ListBox1.Items.Add("Ducados")
+
+        End If
+        If ComboBox1.SelectedIndex = 1 Then
+            ListBox1.Items.Clear()
+            ListBox1.Items.Add("Camel")
+        End If
+        If ComboBox1.SelectedIndex = 2 Then
+            ListBox1.Items.Clear()
+            ListBox1.Items.Add("Malboro")
+        End If
+        If ComboBox1.SelectedIndex = -1 Then
+            ListBox1.Items.Clear()
+        End If
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        If ListBox1.SelectedItem = "Fortuna" Then
+            TextBox2.Text = 20
+            TextBox3.Text = 1
+            TextBox4.Text = 3
+            TextBox5.Text = "5% de descuento"
+            PictureBox1.Image = Image.FromFile(Application.StartupPath & "\Fotos\FORTUNA.jpg")
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        End If
+        If ListBox1.SelectedItem = "Ducados" Then
+            TextBox2.Text = 15
+            TextBox3.Text = 3
+            TextBox4.Text = 5
+            TextBox5.Text = "10% de descuento"
+            PictureBox1.Image = Image.FromFile(Application.StartupPath & "\Fotos\DUCADOS.jpg")
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        End If
+        If ListBox1.SelectedItem = "Camel" Then
+            TextBox2.Text = 20
+            TextBox3.Text = 5
+            TextBox4.Text = 2
+            TextBox5.Text = "5% de descuento"
+            PictureBox1.Image = Image.FromFile(Application.StartupPath & "\Fotos\CAMEL.jpg")
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        End If
+        If ListBox1.SelectedItem = "Malboro" Then
+            TextBox2.Text = 10
+            TextBox3.Text = 5
+            TextBox4.Text = 1
+            TextBox5.Text = "5% de descuento"
+            PictureBox1.Image = Image.FromFile(Application.StartupPath & "\Fotos\MALBORO.jpg")
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+
+
+
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim precioTotal As Double
+        Dim descuento As Boolean
+        If CheckBox1.Checked Then
+            descuento = False
+        Else descuento = True
+        End If
+        Dim cantidad As Integer = NumericUpDown1.Value
+        Dim precio As Integer = TextBox4.Text
+        Dim descuentoMarca As Integer
+
+        If ListBox1.SelectedItem = "Ducados" Then
+            descuentoMarca = 10
+        Else descuentoMarca = 5
+        End If
+
+        Dim descuentoproducto As Double = (cantidad * precio) * descuentoMarca / 100
+
+
+        precioTotal = (cantidad * precio) - descuentoproducto
+
+        If descuento = True Then
+            MsgBox("El precio total es de: " & precioTotal)
+        Else MsgBox("El precio total es de: " & precioTotal * 2)
+        End If
+
+        pedido(x).idPedido = x
+        pedido(x).cantidad = cantidad
+        Dim auxiliar As String = ListBox1.SelectedItem
+        pedido(x).marca = auxiliar
+        pedido(x).total = precioTotal
+
+        x = x + 1
+        ListBox2.Items.Add(x)
+
+    End Sub
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If ListBox2.SelectedIndex = -1 Then
+            MsgBox("Selecciona un pedido para borrar")
+        Else
+            ListBox2.Items.RemoveAt(ListBox2.SelectedIndex)
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim auxiliar As Integer = ListBox2.SelectedIndex
+
+        MsgBox("Pedido:" & auxiliar & " Unidades: " & pedido(auxiliar).cantidad & " Precio: " & pedido(auxiliar).total)
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
