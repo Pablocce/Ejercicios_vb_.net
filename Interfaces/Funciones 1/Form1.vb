a4860672663ab646d6e1a90acbe92b45ca9bcfaa
Pablo <Pablocalvoespinar04@gmail.com>
2022-12-09 17:31:02 +0100

    Ejercicio Examen tabacalera

---
+++ Interfaces/Funciones 1/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Sub saludo1()
+        MsgBox("Hola DAM")
+    End Sub
+
+    Function saludo2()
+        Dim variable As String
+        variable = "Adios DAM"
+        Return variable
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim hola As String
+        hola = saludo2()
+        MsgBox(hola)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        saludo1()
+    End Sub
+End Class
---
+++ Interfaces/Funciones con argumentos/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim resultado As Integer
+
+        Dim valor1, valor2 As Integer
+        valor1 = Val(TextBox1.value)
+    End Sub
+End Class
