3cb2a0a8a59d6a4aea237da59b9dac95a2d3e18b
Pablo <Pablocalvoespinar04@gmail.com>
2023-01-17 20:43:02 +0100

    Ejercicio base de datos

---
+++ Bases_Datos/ejercicios/Ejercicio1_banco/Ejercicio1_banco/Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(51, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Número"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(50, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nombre"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(51, 167)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Saldo"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(462, 57)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(250, 274)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Operaciones"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(6, 226)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(94, 29)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(6, 177)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(94, 29)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Consulta"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(6, 131)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(94, 29)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Modificación"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(6, 87)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(94, 29)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Baja"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 42)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 29)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Alta"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(155, 41)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(155, 108)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Location = New System.Drawing.Point(155, 164)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(50, 380)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(94, 29)
+        Me.Button6.TabIndex = 7
+        Me.Button6.Text = "Pago"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(210, 380)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(94, 29)
+        Me.Button7.TabIndex = 8
+        Me.Button7.Text = "Ingreso"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(468, 380)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(94, 29)
+        Me.Button8.TabIndex = 9
+        Me.Button8.Text = "Salir"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+End Class
---
+++ Bases_Datos/ejercicios/Ejercicio1_banco/Ejercicio1_banco/Form1.vb
@@ -0,0 +1,135 @@
+﻿Imports System.Configuration
+Imports System.Data
+Imports System.Data.OleDb
+Imports Microsoft.Office.Interop.Excel
+
+Public Class Form1
+    Dim conexion As New OleDbConnection()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conexion.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\pablo\Documents\GitHub\Ejercicios_vb_.net\Bases_Datos\database\dbbanco.accdb"
+    End Sub
+
+    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            conexion.Open()
+            Dim consulta = New OleDbCommand("select * from tbcuentas where Numero_Cuenta = " & TextBox1.Text, conexion)
+            Dim datareader As OleDbDataReader
+            datareader = consulta.ExecuteReader
+            If datareader.Read() Then
+                TextBox2.Text = datareader("Nombre")
+                TextBox3.Text = datareader("Saldo")
+            Else
+                MsgBox("no existe un número de cuenta como el introducido")
+            End If
+        End If
+        conexion.Close()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Try
+            conexion.Open()
+            If TextBox1.Text <> Nothing And TextBox2.Text <> Nothing Then
+                Dim cantidad = InputBox("Introduce el dinero a retirar")
+                If cantidad > 0 Then
+                    Dim consulta = New OleDbCommand("update tbcuentas set Saldo = Saldo - " & cantidad & " where Numero_Cuenta = " & TextBox1.Text, conexion)
+                    consulta.ExecuteNonQuery()
+                    Dim aux = TextBox3.Text - cantidad
+                    MsgBox("Dinero retirado")
+                    TextBox3.Text = aux
+
+                Else MsgBox("Introduce una cantidad valida o mayor a cero")
+                End If
+            Else MsgBox("Introduce un numero de cuenta")
+            End If
+        Catch ex As Exception
+            MsgBox("Error no identificado, repita el proceso correctamente")
+        End Try
+
+        conexion.Close()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+
+        Try
+            conexion.Open()
+            If TextBox1.Text <> Nothing And TextBox2.Text <> Nothing Then
+                Dim cantidad = InputBox("Introduce el dinero a ingresar")
+                If cantidad > 0 Then
+                    Dim consulta = New OleDbCommand("update tbcuentas set Saldo = Saldo + " & cantidad & " where Numero_Cuenta = " & TextBox1.Text, conexion)
+                    consulta.ExecuteNonQuery()
+                    MsgBox("Dinero ingresado")
+                    Dim aux = TextBox3.Text + cantidad
+                    TextBox3.Text = aux
+
+                Else MsgBox("Introduce una cantidad valida o mayor a cero")
+                End If
+            Else MsgBox("Introduce un numero de cuenta")
+            End If
+
+        Catch ex As Exception
+            MsgBox("Error desconocido")
+        End Try
+        conexion.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Try
+            conexion.Open()
+            Dim numeroCuenta As Integer = InputBox("Introduce un número de cuenta")
+            Dim Nombre As String = InputBox("Introduce el nombre")
+            Dim Saldo As Integer = InputBox("Introduce el saldo ")
+            Dim insert = New OleDbCommand("insert into tbcuentas values(@a,@b,@c)", conexion)
+            insert.Parameters.AddWithValue("@a", numeroCuenta)
+            insert.Parameters.AddWithValue("@b", Nombre)
+            insert.Parameters.AddWithValue("@c", Saldo)
+            insert.ExecuteNonQuery()
+        Catch ex As Exception
+            MsgBox("Error desconocido")
+        End Try
+        conexion.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            conexion.Open()
+            Dim numeroCuenta As Integer = InputBox("Introduce un número de cuenta que quieres dar de baja")
+            Dim delete = New OleDbCommand("delete from tbcuentas where Numero_Cuenta = " & numeroCuenta, conexion)
+            delete.ExecuteNonQuery()
+            MsgBox("Cuenta número: " & numeroCuenta & " eliminada")
+        Catch ex As Exception
+            MsgBox("Error, no se puede borrar esa cuenta")
+        End Try
+        conexion.Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        conexion.Open()
+        Try
+            Dim numCuenta = InputBox("Introduce el número de cuenta que quieres modificar")
+            Dim opcion As Integer = InputBox("Que quieres modificar: /n Numero de cuenta --> 1  Nombre --> 2 Saldo --> 3")
+            Select Case opcion
+                Case 1
+                    Dim nuevoNumeroCuenta As Integer = InputBox("Introduce el nuevo número de cuenta")
+                    Dim update = New OleDbCommand("update from tbcuentas set Numero_Cuenta = " & nuevoNumeroCuenta & "where Numero_Cuenta = " & numCuenta, conexion)
+                    update.ExecuteNonQuery()
+                    MsgBox("Cuenta modificada")
+                Case 2
+                    Dim nuevoNombre As Integer = InputBox("Introduce el nuevo nombre")
+                    Dim update = New OleDbCommand("update from tbcuentas set Nombre = " & nuevoNombre & " where Numero_Cuenta = " & numCuenta, conexion)
+                    update.ExecuteNonQuery()
+                    MsgBox("Cuenta modificada")
+                Case 3
+                    Dim nuevoSaldo As Integer = InputBox("Introduce el nuevo saldo")
+                    Dim update = New OleDbCommand("update from tbcuentas set Saldo = " & nuevoSaldo & " where Numero_Cuenta = " & numCuenta, conexion)
+                    update.ExecuteNonQuery()
+                    MsgBox("Cuenta modificada")
+                Case Else
+                    MsgBox("Introduce un valor correcto")
+            End Select
+        Catch ex As Exception
+            MsgBox("Error")
+        End Try
+        conexion.Close()
+    End Sub
+End Class
